0fc26cc21d14a74db199f95893373215c0f1608a
Chesterkxng <cgoita00@gmail.com>
2024-08-05 10:03:15 +0000

    changing url paths

--- subscription/urls.py
+++ subscription/urls.py
@@ -3,9 +3,9 @@ from subscription import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('subscriptions/', views.subscriptions, name='subscriptions'),
-    path('addSubscription/', views.addSubscription, name='addSubscription'),
-    path('updateSubscription/', views.updateSubscription, name='updateSubscription'),
-    path('reconductSubscription/', views.reconductSubscription, name='reconductSubscription'),
-    path('deleteSubscription/', views.deleteSubscription, name='deleteSubscription')
+    path('subscriptions/', views.subscriptions, name='subscriptions'), 
+    path('subscription/add', views.addSubscription, name='subscription/add'),
+    path('subscription/update', views.updateSubscription, name='subscription/update'),
+    path('subscription/reconduct', views.reconductSubscription, name='subscription/reconduct'),
+    path('subscription/delete', views.deleteSubscription, name='subscription/delete')
 ]
